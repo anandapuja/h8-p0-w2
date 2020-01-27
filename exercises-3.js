0a26ca7b93a5d7c57703731a3b5db1b80ea5697c
anandapuja <anandapujawandra@gmail.com>
2020-01-27 20:39:43 +0700

    membuat tugas if else

---
+++ exercises-3.js
@@ -0,0 +1,17 @@
+var nama = "";
+var peran = "";
+
+if (nama && peran) {
+    console.log("Nama harus diisi!");
+} else if (nama === 'Mikael' && peran) {
+    console.log('Halo Mikael, Pilih peranmu untuk memulai game!');
+} else if (nama) {
+    console.log("Selamat datang di Dunia Proxytia, Nina" \n
+    "Halo Ksatria Nina, kamu dapat menyerang dengan senjatamu!");
+} else if () {
+    console.log("Selamat datang di Dunia Proxytia, Danu"\n
+    "Halo Tabib Danu, kamu akan membantu temanmu yang terluka.");
+} else if () {
+    console.log ("Selamat datang di Dunia Proxytia, Zero"\n
+    "Halo Penyihir Zero, ciptakan keajaiban yang membantu kemenanganmu!");
+}
