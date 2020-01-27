474cbde3e4018a162b04a9d80c94540119f17434
anandapuja <anandapujawandra@gmail.com>
2020-01-27 20:47:04 +0700

    selesai membuat nama dan peran

--- exercises-3.js
+++ exercises-3.js
@@ -5,13 +5,15 @@ if (nama && peran) {
     console.log("Nama harus diisi!");
 } else if (nama === 'Mikael' && peran) {
     console.log('Halo Mikael, Pilih peranmu untuk memulai game!');
-} else if (nama) {
+} else if (nama === "Nina" && peran === "Ksatria") {
     console.log("Selamat datang di Dunia Proxytia, Nina" \n
     "Halo Ksatria Nina, kamu dapat menyerang dengan senjatamu!");
-} else if () {
+} else if (nama === "Peran" && peran === "Tabib") {
     console.log("Selamat datang di Dunia Proxytia, Danu"\n
     "Halo Tabib Danu, kamu akan membantu temanmu yang terluka.");
-} else if () {
+} else if (nama === "Zero" && peran === "Penyihir") {
     console.log ("Selamat datang di Dunia Proxytia, Zero"\n
     "Halo Penyihir Zero, ciptakan keajaiban yang membantu kemenanganmu!");
+} else {
+    console.log();
 }
