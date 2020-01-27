dc1964cfdcc96911e3ad0c2de15b3c56412c02bb
anandapuja <anandapujawandra@gmail.com>
2020-01-28 00:36:07 +0700

    memulai mengerjakan task 5 minggu 2, soal string

---
+++ exercises-5.js
@@ -0,0 +1,11 @@
+//  1. LET'S FROM A SENTENCE
+
+var word = 'JavaScript';
+var second = 'is';
+var third = 'awesome';
+var fourth = 'and';
+var fifth = 'I';
+var sixth = 'love';
+var seventh = 'it!';
+
+console
