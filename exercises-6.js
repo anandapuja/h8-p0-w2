d8fd02a55b4596ad4af5a4771231986f7fe1e8b5
anandapuja <anandapujawandra@gmail.com>
2020-01-28 05:49:03 +0700

    selesai tugas looping, gage, kelipatan

--- exercises-6.js
+++ exercises-6.js
@@ -32,11 +32,46 @@ for ( i = 20; i >= 2; i-- ) {
 
 //  3. ANGKA GANJIL DAN GENAP
 
+//  Ganjil Genap
+console.log( 'GANJIL - GENAP' );
 for( i = 1; i < 100; i++ ) {
     if ( i % 2 == 0 ) {
-        console.log( 'GENAP' );
+        console.log( i + ' - GENAP' );
     }
     else {
-        console.log( 'GANJIL' );
+        console.log( i + ' - GANJIL' );
+    }
+}
+
+//	Kelipatan 3
+console.log( 'KELIPATAN 3' );
+for( i = 1; i <= 100; i += 2 ) {
+	if( i % 3 !== 0 ) {
+		console.log();
+	}
+	else {
+		console.log( i + ' - KELIPATAN 3' )
+	}
+}
+
+//  Kelipatan 6
+console.log( 'KELIPATAN 6' );
+for( i = 1; i <=100; i += 5 ) {
+    if ( i % 6 !== 0 ) {
+        console.log();
+    }
+    else {
+        console.log( i + ' - KELIPATAN 6' );
+    }
+}
+
+//  Kelipatan 10
+console.log( 'KELIPATAN 10' );
+for( i = 1; i <= 100; i += 9 ) {
+    if( i % 10 !== 0 ) {
+        console.log();
+    }
+    else {
+        console.log( i + ' - KELIPATAN 10' );
     }
 }
