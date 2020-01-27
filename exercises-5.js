3e8b4cd4c287ca97fa1233418eb957c3ca799794
anandapuja <anandapujawandra@gmail.com>
2020-01-28 01:16:29 +0700

    selesai tantangan STRING

--- exercises-5.js
+++ exercises-5.js
@@ -24,3 +24,40 @@ console.log('Second Word: ' + secondWord);
 console.log('Third Word: ' + thirdWord);
 console.log('Fourth Word: ' + fourthWord);
 console.log('Fifth Word: ' + fifthWord);
+
+//  3. BREAKING SENTENCE (AGAIN) USING SUBSTRING
+
+var word3 = 'wow JavaScript is so cool';
+var exampleFirstWord3 = word.substring(0, 3);
+var secondWord3 = word.substring(4, 14)
+var thirdWord3 = word.substring(15, 17);
+var fourthWord3 = word.substring(18, 20);
+var fifthWord3 = word.substring(21, 25);
+
+console.log('First Word: ' + exampleFirstWord3);
+console.log('Second Word: ' + secondWord3);
+console.log('Third Word: ' + thirdWord3);
+console.log('Fourth Word: ' + fourthWord3);
+console.log('Fifth Word: ' + fifthWord3);
+
+//	4. BREAKING SENTENCE (YET AGAIN) AND COUNT EACH LENGTH
+
+var word4 = 'wow JavaScript is so cool';
+var exampleFirstWord4 = word.substring(0, 3);
+var secondWord4 = word.substring(4, 14);
+var thirdWord4 = word.substring(15, 17);
+var fourthWord4 = word.substring(18, 20);
+var fifthWord4 = word.substring(21, 25);
+
+var firstWordLength = exampleFirstWord4.length;
+var secondWordLength = secondWord4.length;
+var thirdWordLength = thirdWord4.length;
+var fourthWordLength = fourthWord4.length;
+var fifthWordLength = fifthWord4.length;
+// create new variables around here
+
+console.log('First Word: ' + exampleFirstWord + ', with length: ' + firstWordLength);
+console.log('Second Word: ' + secondWord + ', with length: ' + secondWordLength);
+console.log('Third Word: ' + thirdWord + ', with length: ' + thirdWordLength);
+console.log('Fourth Word: ' + fourthWord + ', with length: ' + fourthWordLength);
+console.log('Fifth Word: ' + fifthWord + ', with length: ' + fifthWordLength);
