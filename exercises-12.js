8c624639316696ea427af1c95b62e25d91e56df8
anandapuja <anandapujawandra@gmail.com>
2020-01-29 23:38:37 +0700

    selesai konversi menit ke jam

--- exercises-12.js
+++ exercises-12.js
@@ -1,7 +1,17 @@
 // Logic Challenge - Konversi Menit
 
 function konversiMenit(menit) {
-    var jam = menit / 60;
+    var jam = menit / 60; //  Ukur hasil bagi antara parameter dengan jumlah menit (60) dalam 1 jam
+    var iJam = Math.floor(jam); // Untuk format JAM, ambil hanya angka bulat hasil bagi jika parameter tidak habis dibagi 60
+    var modJam = menit % 60; // Tampung nilai hasil sisa bagi parameter dengan jumlah menit (60)
+    var formatModJam = modJam.toString().length; // Hitung jumlah digit number dari modJam untuk dijadikan kondisi
+    
+    // Buat kondisi untuk mengatur format MENIT (setelah :)
+    if (formatModJam === 1) { // Jika jumlah digit menit/ modJam hanya 1 digit, tambahkan 0 didepan angkanya
+      modJam = "0" + modJam;
+    }
+
+    return `${iJam}:${modJam}`; //  Kembalikan format JAM : format MENIT
   }
   
   // TEST CASES
