cdb84ee68601727a738363f3973ed29995bf8641
anandapuja <anandapujawandra@gmail.com>
2020-01-29 12:52:04 +0700

    mulai mengerjakan task 13 week 2 X dan O

---
+++ exercises-13.js
@@ -0,0 +1,12 @@
+//  Logic Challenge - X dan O
+
+function xo(str) {
+    // you can only write your code here!
+  }
+  
+  // TEST CASES
+  console.log(xo('xoxoxo')); // true
+  console.log(xo('oxooxo')); // false
+  console.log(xo('oxo')); // false
+  console.log(xo('xxxooo')); // true
+  console.log(xo('xoxooxxo')); // true
